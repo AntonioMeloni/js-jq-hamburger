bfedcff4d390d67dce8556583ab87958198a5e7d
Antonio <ameloni9@gmail.com>
2020-02-27 16:22:08 +0100

    Hamburger onClick!

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".fa-bars").click(function () {
+    $(".hamburger-menu").fadeIn(400);
+    // $(".hamburger-menu").css("display","block");
+})
+$(".close").click(function () {
+    $(".hamburger-menu").hide(400);
+    // $(".hamburger-menu").css("display","none");
+})
